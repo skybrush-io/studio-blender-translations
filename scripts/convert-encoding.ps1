ec4e5d4985f811ba23b85139a1f6cda3120450b0
István Donkó <istvan.donko@gmail.com>
2026-04-28 13:15:17 +0200

    fix: use absolute paths when converting encodings
    Explicitly get `.FullName` to avoid issues caused by relative paths and
    the weird mismatch between `$PWD` and `[Environment]::CurrentDirectory`.

--- scripts/convert-encoding.ps1
+++ scripts/convert-encoding.ps1
@@ -1,9 +1,15 @@
 #!/usr/bin/env pwsh
 
+<#
+  .DESCRIPTION
+  Call this script from the root directory of the repository to
+  strip the byte order marks from all files in the `po` folder!
+#>
+
 $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
-foreach ($f in Get-ChildItem -Recurse) {
+foreach ($f in Get-ChildItem "po") {
   if (-Not $f.PSIsContainer) {
-    $text = [System.IO.File]::ReadAllText($f)
-    [System.IO.File]::WriteAllText($f, $text, $Utf8NoBomEncoding)
+    $text = [System.IO.File]::ReadAllText($f.FullName)
+    [System.IO.File]::WriteAllText($f.FullName, $text, $Utf8NoBomEncoding)
   }
 }
